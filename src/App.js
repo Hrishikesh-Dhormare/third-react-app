23db82001a70bef58323a101530cdd53d4e2bade
Hrishikesh <dhormarehrishi10@gmail.com>
2023-06-16 00:30:54 +0530

    multiple input box

--- src/App.js
+++ src/App.js
@@ -3,53 +3,51 @@ import { useRef, useState } from "react";
 function App() {
   return (
     <>
-      <h1>Use of Ref Hook</h1>
-      <ListDemo />
+      <h1>My Todo</h1>
+      <MyTodo />
     </>
   );
 }
 
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["WPT Module"]);
+function MyTodo() {
+  let [todo, setTodo] = useState({ task: "", description: "" });
 
-  let addItemAction = () => {
-    let inputValue = inputRef.current.value; //value from input text
-    let newList = [inputValue, ...list]; //Adding input value to the existing Value
+  let handleChnageTaskAction = (e) => {
+    let newTodo = { ...todo, task: e.target.value };
+    setTodo(newTodo);
+  };
 
-    setList(newList);
+  let handleChangeDescriptionAction = (e) => {
+    // console.log(e.target);
+    let newTodo = { ...todo, description: e.target.value };
+    setTodo(newTodo);
+  };
 
-    inputRef.current.value = "";
+  let addTodoAction = () => {
+    console.log(todo);
+    // TODO :: Save this do DB
   };
 
   return (
     <>
       <input
+        className="form-control"
         type="text"
-        id="id1"
-        ref={inputRef}
-        placeholder="Enter user input..."
+        placeholder="Enter task"
+        value={todo.task}
+        onChange={handleChnageTaskAction}
       />
-      <input type="button" value="Add New Item" onClick={addItemAction} />
-      {list.map((item) => (<MessageDemo message={item}/>))}
-    </>
-  );
-}
 
-function MessageDemo({ message }) {
-  return (
-    <>
-      <h1>Hello {message}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem quas sequi tenetur quo odit cupiditate assumenda voluptas
-        quibusdam blanditiis voluptatem quis mollitia eaque molestiae animi,
-        provident eligendi facilis incidunt.
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
-      </div>
+      <textarea
+        className="form-control"
+        cols="30"
+        rows="3"
+        placeholder="Enter Description"
+        value={todo.description}
+        onChange={handleChangeDescriptionAction}
+      ></textarea>
+
+      <input type="button" value="Add Todo" onClick={addTodoAction} />
     </>
   );
 }
